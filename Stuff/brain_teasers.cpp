27fc89b1dc1f61044b4ceb2810ee1939efb8c343
pg <paul.quant@gmail.com>
2012-11-12 11:25:49 +0000

    brain teaser algorithms
    intersection of lists

---
+++ Stuff/brain_teasers.cpp
@@ -0,0 +1,9 @@
+//
+//  brain_teasers.cpp
+//  Stuff
+//
+//  Created by Paul Gribelyuk on 11/12/12.
+//  Copyright (c) 2012 paulg. All rights reserved.
+//
+
+#include "brain_teasers.h"
---
+++ Stuff/brain_teasers.h
@@ -0,0 +1,14 @@
+//
+//  brain_teasers.h
+//  Stuff
+//
+//  Created by Paul Gribelyuk on 11/12/12.
+//  Copyright (c) 2012 paulg. All rights reserved.
+//
+
+#ifndef __Stuff__brain_teasers__
+#define __Stuff__brain_teasers__
+
+#include <iostream>
+
+#endif /* defined(__Stuff__brain_teasers__) */
